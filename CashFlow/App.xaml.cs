e92eebcc21fb21d6fcad8698f495edb47146e80b
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-04-17 23:23:22 +0200

    Agregar archivos de proyecto.

---
+++ CashFlow/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlow;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CashFlow/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CashFlow;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
