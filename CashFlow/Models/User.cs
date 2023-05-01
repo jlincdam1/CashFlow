1bdd3f30176941e58566e2fedf567ae627dda8d2
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-01 22:26:29 +0200

    Añadido el modelo para los datos del usuario

---
+++ CashFlow/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surnames { get; set; }
+        public float InitCapital { get; set; }
+        public float Capital { get; set; }
+        public float MensualEarning { get; set; }
+    }
+}
