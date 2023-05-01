155f829ed28b0987cf756ef4a5428f759cda178b
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-01 22:46:22 +0200

    Añadido clase para gestionar la tabla de User

---
+++ CashFlow/Data/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using CashFlow.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Data
+{
+    public class UserRepository
+    {
+        string _dbPath;
+        private SQLiteConnection _connection;
+
+        public UserRepository(string dbPath)
+        {
+            _dbPath = dbPath;
+        }
+
+        public void Init()
+        {
+            _connection = new SQLiteConnection(_dbPath);
+            _connection.CreateTable<User>();
+        }
+
+        public User Get(int id)
+        {
+            Init();
+            return _connection.Table<User>().Where(o => o.Id == id).FirstOrDefault();
+        }
+    }
+}
