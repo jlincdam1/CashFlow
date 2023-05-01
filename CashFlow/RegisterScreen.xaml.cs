fa38ac193e09dd7bec216b3c25d5278421f1e0ed
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-01 21:19:04 +0200

    Modificado la pantalla de registro para activar y desactivar el botón según los datos que se ha introducido

--- CashFlow/RegisterScreen.xaml.cs
+++ CashFlow/RegisterScreen.xaml.cs
@@ -12,5 +12,33 @@ public partial class RegisterScreen : ContentPage
         await btnRegistro.ScaleTo(0.8, 50);
         await btnRegistro.ScaleTo(1, 50);
     }
+
+	private bool entriesRight()
+	{
+		return !string.IsNullOrWhiteSpace(nombre.Text) && !string.IsNullOrWhiteSpace(apellidos.Text) && 
+            float.TryParse(capitalI.Text, out float result);
+	}
+
+    private void on_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (entriesRight() && string.IsNullOrWhiteSpace(gananciaM.Text))
+        {
+            btnRegistro.Opacity = 0.8;
+            btnRegistro.IsEnabled = true;
+        }
+        else
+        {
+            if(entriesRight() && float.TryParse(gananciaM.Text, out float result))
+            {
+                btnRegistro.Opacity = 0.8;
+                btnRegistro.IsEnabled = true;
+            }
+            else
+            {
+                btnRegistro.Opacity = 0.2;
+                btnRegistro.IsEnabled = false;
+            }
+        }
+    }
 }
 
