69e370228f6d695fb1ddf90e92002977e2f2a7a3
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-02 22:17:20 +0200

    Código para añadir el usuario al darle a Registrarse

--- CashFlow/RegisterScreen.xaml.cs
+++ CashFlow/RegisterScreen.xaml.cs
@@ -1,16 +1,57 @@
-﻿namespace CashFlow;
+﻿using CashFlow.Models;
+using CashFlow.Services;
+
+namespace CashFlow;
 
 public partial class RegisterScreen : ContentPage
 {
+    private readonly IUserService _userService;
 	public RegisterScreen()
 	{
 		InitializeComponent();
+        _userService = new UserService();
 	}
 
 	async void Registrarse(object sender, EventArgs e)
 	{
         await btnRegistro.ScaleTo(0.8, 50);
         await btnRegistro.ScaleTo(1, 50);
+        User user = new User();
+
+        if (string.IsNullOrWhiteSpace(gananciaM.Text))
+        {
+            user = new User
+            {
+                Id = 1,
+                Name = nombre.Text,
+                Surnames = apellidos.Text,
+                InitCapital = float.Parse(capitalI.Text),
+                Capital = float.Parse(capitalI.Text)
+            };
+        }
+        else
+        {
+            user = new User
+            {
+                Id = 1,
+                Name = nombre.Text,
+                Surnames = apellidos.Text,
+                InitCapital = float.Parse(capitalI.Text),
+                Capital = float.Parse(capitalI.Text),
+                MensualEarning = float.Parse(gananciaM.Text)
+            };
+        }
+
+        var response = await _userService.AddUser(user);
+
+        if(response > 0)
+        {
+            await DisplayAlert("Éxito", "Añadido correctamente", "Aceptar");
+        }
+        else
+        {
+            await DisplayAlert("Error", "Error al añadir", "Aceptar");
+        }
     }
 
 	private bool entriesRight()
