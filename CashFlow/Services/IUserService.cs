fba154292f15f41422b21a34c1af0759fddb5739
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-02 21:22:07 +0200

    Añadido el servicio para User junto a su interfaz

---
+++ CashFlow/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Services
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetUser();
+
+        Task<bool> AddUser(User user);
+
+        Task<bool> DeleteUser(User user);
+        Task<bool> UpdateUser(User user);
+    }
+}
---
+++ CashFlow/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Services
+{
+    internal class UserService : IUserService
+    {
+        public Task<bool> AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
