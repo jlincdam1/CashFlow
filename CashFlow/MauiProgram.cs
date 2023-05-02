e7c47cb82b3d1e37336f52063704193205ddcca3
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-02 21:38:04 +0200

    Añadido servicio a las dependecias

--- CashFlow/MauiProgram.cs
+++ CashFlow/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CashFlow.Services;
+using Microsoft.Extensions.Logging;
 
 namespace CashFlow;
 
@@ -13,7 +14,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.Services.AddSingleton<IUserService, UserService>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
