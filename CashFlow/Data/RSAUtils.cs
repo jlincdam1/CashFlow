2034ae29d49ef72775cd1ad5c150f8761a0e7522
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-04 22:21:28 +0200

    Añadido utilidades para RSA

---
+++ CashFlow/Data/RSAUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Data
+{
+    public class RSAUtils
+    {
+        static string pubKeyStr;
+        static string privKeyStr;
+
+        static string Encriptar(string str)
+        {
+            RSAServices rsa = new RSAServices();
+            RSAServices rsa2 = new RSAServices();
+
+            pubKeyStr = rsa.GetPublicKey();
+            privKeyStr = rsa.GetPrivateKey();
+
+            var pubKey = RSAServices.PublicParametersFromXml(pubKeyStr);
+
+            string resulEncrit = rsa2.Encrypt(str, pubKey);
+
+            return resulEncrit;
+        }
+
+        static string Desencriptar(string privkeyStr, string msjEncriptado)
+        {
+            RSAServices rsa = new RSAServices();
+
+            var privKey = RSAServices.PublicParametersFromXml(privkeyStr);
+
+            string resulDecript = rsa.Decrypt(msjEncriptado, privKey);
+
+            return resulDecript;
+        }
+    }
+}
