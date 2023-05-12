ed0df78bd701feed7158c12c00761b1b0e1b35ba
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-13 00:33:28 +0200

    Añadido código para obtener los gastos e inversiones para la gráfica de pizza

--- CashFlow/GraphicsData/ActivitiesModel.cs
+++ CashFlow/GraphicsData/ActivitiesModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CashFlow.Data;
+using CashFlow.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,57 @@ using System.Threading.Tasks;
 
 namespace CashFlow.GraphicsData
 {
-    class ActivitiesModel
+    public class ActivitiesModel
     {
+        public readonly CashFlowDatabase database;
+        List<Activities> activities = new List<Activities>();
+        public float gastos;
+        public float inversiones;
+
+        public IReadOnlyList<Movimiento> MovimientosPie { get; }
+        public ActivitiesModel() 
+        {
+            gastos = 0;
+            inversiones = 0;
+            database = new CashFlowDatabase();
+            LoadActivitiesAsync();
+
+            if (activities.Count > 0)
+            {
+                foreach (Activities activity in activities)
+                {
+                    if(activity.ActType == "Inversión")
+                    {
+                        inversiones += activity.Quantity;
+                    }
+                    else
+                    {
+                        gastos += activity.Quantity;
+                    }
+                }
+            }
+
+            MovimientosPie = new List<Movimiento>()
+            {
+                new Movimiento("Gastos", gastos),
+                new Movimiento("Inversiones", inversiones)
+            };
+        }
+
+        private async void LoadActivitiesAsync()
+        {
+            activities = await database.GetActivitiesAsync();
+        }
+    }
+    public class Movimiento
+    {
+        public string MovName { get; }
+        public float Quantity { get; }
+
+        public Movimiento(string actName, float quant)
+        {
+            this.MovName = actName;
+            this.Quantity = quant;
+        }
     }
 }
