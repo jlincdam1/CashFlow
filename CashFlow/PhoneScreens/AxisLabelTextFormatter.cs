257adcfa9b39c97830c8583e57b9994762f50c71
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-13 23:44:52 +0200

    Añadido formateo para la fecha

---
+++ CashFlow/PhoneScreens/AxisLabelTextFormatter.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Maui.Charts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.PhoneScreens
+{
+    public class AxisLabelTextFormatter : IAxisLabelTextFormatter
+    {
+        public string Format(object value)
+        {
+            return ((DateTime)value).ToString("MM");
+        }
+    }
+}
