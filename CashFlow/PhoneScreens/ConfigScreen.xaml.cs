49d233c52c4405362dddb92144592e18cb0e48e6
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-14 13:51:49 +0200

    Añadido código para cargar la información del usuario

--- CashFlow/PhoneScreens/ConfigScreen.xaml.cs
+++ CashFlow/PhoneScreens/ConfigScreen.xaml.cs
@@ -1,9 +1,31 @@
+using CashFlow.Data;
+using CashFlow.Models;
+using System.Globalization;
+
 namespace CashFlow.PhoneScreens;
 
 public partial class ConfigScreen : ContentPage
 {
-	public ConfigScreen()
+    private readonly CashFlowDatabase database;
+    public ConfigScreen()
 	{
 		InitializeComponent();
-	}
+        NavigationPage.SetHasNavigationBar(this, false);
+        database = new CashFlowDatabase();
+        LoadData();
+    }
+    protected override bool OnBackButtonPressed()
+    {
+        return true;
+    }
+
+    private async void LoadData()
+    {
+        User user = await database.GetUserAsync();
+        nombre.Text = RSAUtils.Desencriptar(RegisterScreen.namePrivKey, user.Name);
+        apellidos.Text = RSAUtils.Desencriptar(RegisterScreen.surnamesPrivKey, user.Surnames);
+        capital.Text = user.Capital.ToString(CultureInfo.InvariantCulture);
+        gananciaM.Text = user.MensualEarning.ToString(CultureInfo.InvariantCulture);
+    }
+
 }
