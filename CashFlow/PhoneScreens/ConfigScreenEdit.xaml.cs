7252f297ea28dad9d883ceaff6610998fcf8e0a8
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-14 14:21:58 +0200

    Añadido método para cargar los datos en la edición

--- CashFlow/PhoneScreens/ConfigScreenEdit.xaml.cs
+++ CashFlow/PhoneScreens/ConfigScreenEdit.xaml.cs
@@ -1,9 +1,24 @@
+using CashFlow.Data;
+using CashFlow.Models;
+using System.Globalization;
+
 namespace CashFlow.PhoneScreens;
 
 public partial class ConfigScreenEdit : ContentPage
 {
-	public ConfigScreenEdit()
+    private readonly CashFlowDatabase database;
+    public ConfigScreenEdit()
 	{
 		InitializeComponent();
-	}
+        database = new CashFlowDatabase();
+        LoadData();
+    }
+
+    private async void LoadData()
+    {
+        User user = await database.GetUserAsync();
+        name.Text = RSAUtils.Desencriptar(RegisterScreen.namePrivKey, user.Name);
+        surnames.Text = RSAUtils.Desencriptar(RegisterScreen.surnamesPrivKey, user.Surnames);
+        mensualEarning.Text = user.MensualEarning.ToString(CultureInfo.InvariantCulture);
+    }
 }
