06516620cd5ce1d408d9992026bef272ff7f9aba
Jiacheng Lin <90404393+jlincdam1@users.noreply.github.com>
2023-05-15 22:19:10 +0200

    Añadido picker para el año en las gráficas de BarSeries

--- CashFlow/PhoneScreens/ConfigScreen.xaml.cs
+++ CashFlow/PhoneScreens/ConfigScreen.xaml.cs
@@ -68,7 +68,7 @@ public partial class ConfigScreen : ContentPage
             await database.DeleteUserAsync();
             await database.DeleteAllActivities();
             await DisplayAlert("Elminado todo", "Se ha eliminado el usuario junto a todos sus movimientos", "Aceptar");
-            await Navigation.PushAsync(new RegisterScreen());
+            Application.Current.Quit();
         }
     }
 }
